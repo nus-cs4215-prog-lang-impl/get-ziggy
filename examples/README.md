af2a079406c12fdc7a11089eba9a073f64d59699
Chun Yu <lamchunyu00@gmail.com>
2025-03-20 12:21:32 +0800

    refactor: clean up generated parser code

---
+++ examples/README.md
@@ -0,0 +1,1 @@
+Rust examples, taken from https://github.com/antlr/grammars-v4/tree/master/rust/examplesk
--- parser/testing_script.py
+++ parser/testing_script.py
@@ -11,8 +11,8 @@ from RustLexer import RustLexer
 from RustParser import RustParser
 import traceback
 import time
-examples_clean = [l.path for l in os.scandir('../examples') if l.name.endswith('.rs')]
-examples_tree = [l.path for l in os.scandir('../examples') if l.name.endswith('.rs.tree')]
+examples_clean = [l.path for l in os.scandir('../examples/quick') if l.name.endswith('.rs')]
+examples_tree = [l.path for l in os.scandir('../examples/quick') if l.name.endswith('.rs.tree')]
 
 def test_grammar(filepath, should_pass = True):
     inputstream = FileStream(filepath,encoding='utf-8')
@@ -20,16 +20,16 @@ def test_grammar(filepath, should_pass = True):
     lexer = RustLexer(inputstream, output)
     tokenstream = CommonTokenStream(lexer)
     parser = RustParser(tokenstream, output)
-    did_pass = None 
+    did_pass = None
     start, end = None, None
     try:
         start = time.time_ns()
         print(parser.crate().toStringTree(parser.ruleNames))
         end = time.time_ns()
-        did_pass = True 
+        did_pass = True
     except Exception as e:
         end = time.time_ns()
-        did_pass = False 
+        did_pass = False
         output.write('\n'*2)
         output.write(' *'*10)
         output.write(' EXCEPTION ')
@@ -41,7 +41,7 @@ def test_grammar(filepath, should_pass = True):
     did_pass = result.replace(" ",'').replace('\n','') == ''
     d = ""
     if end != None:
-        d = f'{(end - start)/ 1_000_000_000}s' 
+        d = f'{(end - start)/ 1_000_000_000}s'
     print(f"{filepath} -> {'pass' if did_pass else 'fail'} {d}")
     print(result)
 
