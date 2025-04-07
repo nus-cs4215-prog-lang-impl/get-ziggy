d4fc9ecb5de2b0bd5277f7d5b2be120d6d2de8d3
Chun Yu <lamchunyu00@gmail.com>
2025-04-07 20:36:06 +0800

    refactor: removed duplicate example folder

--- parser/examples/README.md
+++
@@ -1,1 +0,0 @@
-Rust examples, taken from https://github.com/antlr/grammars-v4/tree/master/rust/examplesk
--- parser/examples/quick/v1_56_0_open_range.rs
+++
@@ -1,6 +0,0 @@
-fn main(){
-    match x as u32 {
-        0 => println!("zero!"),
-        1.. => println!("positive number!"),
-    };
-}
